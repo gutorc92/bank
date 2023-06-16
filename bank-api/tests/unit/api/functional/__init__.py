08203575ea10b5a4e29a5cf21ae123f1ebdbfc7e
Gustavo Coelho <gutorc@hotmail.com>
2023-06-16 20:02:58 -0300

    feat: fake person factory
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- bank-api/tests/unit/api/functional/__init__.py
+++ bank-api/tests/unit/api/functional/__init__.py
@@ -1,6 +1,38 @@
 import pytest
-
+import factory
 from app import create_app
+from app.models.models import Person
+from functools import partial
+from typing import Any, Dict
+from factory import Factory
+from factory.base import StubObject
+
+def generate_dict_factory(factory: Factory):
+    def convert_dict_from_stub(stub: StubObject) -> Dict[str, Any]:
+        stub_dict = stub.__dict__
+        for key, value in stub_dict.items():
+            if isinstance(value, StubObject):
+                stub_dict[key] = convert_dict_from_stub(value)
+        return stub_dict
+
+    def dict_factory(factory, **kwargs):
+        stub = factory.stub(**kwargs)
+        stub_dict = convert_dict_from_stub(stub)
+        return stub_dict
+
+    return partial(dict_factory, factory)
+
+class PersonFactory(factory.Factory):
+    class Meta:
+        model = Person
+    
+    name = factory.Faker('first_name')
+    surname = factory.Faker('last_name')
+    id_gov = '08798754123'
+
+@pytest.fixture(scope='session', autouse=True)
+def faker_session_locale():
+    return ['pt_BR']
 
 @pytest.fixture(autouse=True, scope="module")
 def client():
--- bank-api/tests/unit/api/functional/test_person.py
+++ bank-api/tests/unit/api/functional/test_person.py
@@ -1,21 +1,20 @@
 import json
-from tests.unit.api.functional import client
+from tests.unit.api.functional import client, PersonFactory, generate_dict_factory
+
+
 
 def test_person(client):
     response = client.get('/api/person/')
     assert response.status_code == 200
 
-def test_create_a_person(client):
+def test_create_a_person(client, faker):
     mimetype = 'application/json'
     headers = {
         'Content-Type': mimetype,
         'Accept': mimetype
     }
-    person = {
-        'name': 'Teste',
-        'surname': 'Testando',
-        'id_gov': '08798754123'
-    }
+    PersonDict = generate_dict_factory(PersonFactory)
+    person = PersonDict()
     response = client.post('/api/person/', data=json.dumps(person), headers=headers)
     assert response.status_code == 200
     assert response.data
